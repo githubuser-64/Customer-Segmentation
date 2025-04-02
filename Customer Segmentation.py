a70e7696ce0dfcee897817d4f3f30f825f29c6dc
githubuser-64 <roadrashfifa21@gmail.com>
2025-04-02 23:43:44 +0530

    Update Customer Segmentation.py

--- Customer Segmentation.py
+++ Customer Segmentation.py
@@ -49,4 +49,17 @@ def describe(data):
 
 data = pd.read_excel('marketing_campaign1.xlsx')
 
-st.write(data)
+option = st.selectbox(
+    'Show',
+    ('head', 'tail', 'sample'))
+
+number = st.slider('number of rows', 1, 100, 5)
+
+
+if option == 'head':
+    st.write(data.head(number))
+elif option == 'tail':
+    st.write(data.tail(number))
+elif option == 'sample':
+    st.write(data.sample(number))
+
